9a49044a7518609a1f189b9075bbea3afe9fe35c
Isaura Figueiredo <156593603+IsauraFigueiredo@users.noreply.github.com>
2024-01-21 17:48:48 +0000

    Criar a Classe Conversoes e mover o método
    O método ConverterTemperatura()  foi removido da classe Aritmética e colocado na classe Conversoes uma vez que implementa uma operação que não se adequa à classe.

--- Aritmetica.cs
+++ Aritmetica.cs
@@ -9,20 +9,12 @@ namespace Calculos
  /// Implementa operações aritméticas.
  /// </summary>
 
-    static class  Aritmetica
-    {/// <summary>
-    /// Tipo de conversão de Temperatura a executar.
-    /// </summary>
-        public enum ConversaoTemperatura
-        {
-            Nulo = 0,
-            CelsiusFahrenheit,
-            FahrenheitCelsius
-        }
-       /// <summary>
-       /// Operação Soma
-       /// </summary>
-       /// <returns>Retorna a soma de dois números.</returns>
+    static class Aritmetica
+    {
+        /// <summary>
+        /// Operação Soma
+        /// </summary>
+        /// <returns>Retorna a soma de dois números.</returns>
         public static int Somar(int x, int y)
         {
            return x + y;
@@ -36,18 +28,6 @@ namespace Calculos
             return x - y;
         }
 
-        public static double ConverterTemperatura(ConversaoTemperatura conversao, double temperatura)
-        {
-            if (conversao == ConversaoTemperatura.CelsiusFahrenheit)
-            {
-                return (temperatura * 1.800 + 32);
-            } 
-            else if(conversao==ConversaoTemperatura.FahrenheitCelsius )
-            {
-                return ((temperatura-32 )/1.800);
-            }
-           
-            return -1;  
-        }
+       
     }
 }
---
+++ Conversoes.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculos
+{/// <summary>
+/// Implementa operações de conversão.
+/// </summary>
+    static class Conversoes
+    {
+        /// <summary>
+        /// Tipo de conversão de Temperatura a executar.
+        /// </summary>
+       public enum ConversaoTemperatura
+       {
+            Nulo = 0,
+            CelsiusFahrenheit,
+            FahrenheitCelsius
+       }
+        /// <summary>
+        /// Conversaode temperaturas
+        /// </summary>
+        /// <param name="conversao">A conversão a efetua.r</param>
+        /// <param name="temperatura">A temperatura a converter.</param>
+        /// <returns>Retorna o resuktado da conversão.</returns>
+       public static double ConverterTemperatura(ConversaoTemperatura conversao, double temperatura)
+       {
+           if (conversao == ConversaoTemperatura.CelsiusFahrenheit)
+           {
+               return (temperatura * 1.800 + 32);
+           }
+           else if (conversao == ConversaoTemperatura.FahrenheitCelsius)
+           {
+                return ((temperatura - 32) / 1.800);
+           }
+
+           return -1;
+       }
+    }
+}
